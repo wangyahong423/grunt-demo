cae75a7f30f8c25f55311815cf4e7f6511c08722
wangyahong423 <2976716563@qq.com>
2020-03-13 21:26:44 +0800

    eslint

--- rectangle.js
+++ rectangle.js
@@ -6,27 +6,27 @@
  * @returns 近似处理后的数 
  */
 function roundFractional(x, n) {
-    return Math.round(x * Math.pow(10, n)) / Math.pow(10, n);
+  return Math.round(x * Math.pow(10, n)) / Math.pow(10, n);
 }
 
 $(function () {
-    // get dom elem
-    var $width = $('#width'),
-        $height = $('#height'),
-        $btnCal = $('#calculate'),
-        $perimeter = $('#perimeter'),
-        $area = $('#area');
-    /*calc button click event */
-    $btnCal.click(function () {
-        // get value
-        var w = Number($width.val()),
-            h = Number($height.val());
-        // calculate
-        var p = roundFractional(2 * (w + h),2) ,
-            a = roundFractional(w * h,2) ;
-        // output
-        $perimeter.val(p);
-        $area.val(a);
-    })
+  // get dom elem
+  var $width = $('#width'),
+      $height = $('#height'),
+      $btnCal = $('#calculate'),
+      $perimeter = $('#perimeter'),
+      $area = $('#area');
+  /*calc button click event */
+  $btnCal.click(function () {
+    // get value
+    var w = Number($width.val()),
+        h = Number($height.val());
+    // calculate
+    var p = roundFractional(2 * (w + h),2) ,
+        a = roundFractional(w * h,2) ;
+    // output
+    $perimeter.val(p);
+    $area.val(a);
+  });
 
 });
